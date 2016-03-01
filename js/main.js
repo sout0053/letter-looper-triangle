401553dda7478831de330a7484e4723764e2d917
sout0053 <maxwell.southwood@outlook.com>
2016-03-01 16:56:44 -0500

    Fixed javascript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-var letter = prompt('Enter a letter');
-var num = 1;
 
-do {
-  for (i = 0; i < num; i++) {
-    document.write(letter);
+var letter = prompt('Enter a letter.');
+
+
+var triangle = function (name, times) {
+  var i;
+
+  for (i = 0; i < times; i++) {
+    document.write(name);
   }
-  document.write('<br>');
-  num++;
-} while (num <= 10);
+
+   document.write('<br>');
+
+};
+
+
+triangle (letter, 1);
+triangle (letter, 2);
+triangle (letter, 3);
+triangle (letter, 4);
+triangle (letter, 5);
+triangle (letter, 6);
+triangle (letter, 7);
+triangle (letter, 8);
+triangle (letter, 9);
