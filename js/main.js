5929f3cb29e48008ca2fa8323d0a110bf10f0e41
sout0053 <maxwell.southwood@outlook.com>
2016-03-01 16:31:37 -0500

    Added javascript

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var letter = prompt('Enter a letter.');
+
+var counter = function (name, times) {
+  var i;
+
+  for (i = 0; 1 < times; i++) {
+    document.write('<br>')
+  }
+
+  document.write('<br>');
+
+};
+
+counter (letter, 1);
+counter (letter, 2);
+counter (letter, 3);
+counter (letter, 4);
+counter (letter, 5);
+counter (letter, 6);
+counter (letter, 7);
+counter (letter, 8);
+counter (letter, 9);
